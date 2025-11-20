4dc0e26948499c6f581f36548e3179f7e2a0a084
Falcon23956 <kioliemmanuel@gmail.com>
2025-11-20 17:54:49 +0300

    Full reset + installed new crestvista_full_features site

--- admin.js
+++
@@ -1,9 +0,0 @@
-// Protect admin page
-if (localStorage.getItem("crestvista_auth") !== "true") {
-    window.location.href = "login.html";
-}
-
-function logout() {
-    localStorage.removeItem("crestvista_auth");
-    window.location.href = "login.html";
-}
